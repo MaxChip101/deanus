7bdd7e310552173ae4bf0da3da56ba2daa2cb873
MaxChip101 <1Max0Lag1@gmail.com>
2024-09-07 13:19:05 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string tokenify(string content) {
+    for (int i = 0; i < content.length(); i++) {
+
+    }
+
+    return "real";
+}
+
+int main() {
+    tokenify("real");
+    std::cout << "Hello, World!" << "\n";
+    return 0;
+}
