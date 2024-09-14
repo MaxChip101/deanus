fd8f2377b44e0542a07e8b7070872afa4993be0d
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2024-09-14 00:56:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,151 +1,216 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
+
+unordered_map<char, string> key_tokens {
+    {'!', "=[EXCLAMATION]="},
+    {'@', "=[AT]="},
+    {'#', "=[HASHTAG]="},
+    {'$', "=[DOLLAR]="},
+    {'%', "=[PERCENT]="},
+    {'^', "=[CARAT]="},
+    {'&', "=[AMPERSAND]="},
+    {'*', "=[MULTIPLY]="},
+    {'(', "=[LEFT_PARENTHESIS]="},
+    {')', "=[RIGHT_PARENTHESIS]="},
+    {'-', "=[MINUS]="},
+    {'+', "=[PLUS]="},
+    {'=', "=[EQUAL]="},
+    {'{', "=[LEFT_BRACE]="},
+    {'}', "=[RIGHT_BRACE]="},
+    {'[', "=[LEFT_BRACKET]="},
+    {']', "=[RIGHT_BRACKET]="},
+    {'\\', "=[BACKSLASH]="},
+    {':', "=[COLON]="},
+    {';', "=[SEMI]="},
+    {'\"', "=[QUOTE]="},
+    {'\'', "=[APOSTROPHE]="},
+    {'<', "=[LESS_THAN]="},
+    {',', "=[COMMA]="},
+    {'>', "=[GREATER_THAN]="},
+    {'.', "=[PERIOD]="},
+    {'?', "=[QUESTION]="},
+    {'/', "=[SLASH]="},
+    {' ', "=[SPACE]="},
+};
+
+unordered_map<string, char> get_key_tokens {
+    {"=[EXCLAMATION]=", '!'},
+    {"=[AT]=", '@'},
+    {"=[HASHTAG]=", '#'},
+    {"=[DOLLAR]=", '$'},
+    {"=[PERCENT]=", '%'},
+    {"=[CARAT]=", '^'},
+    {"=[AMPERSAND]=", '&'},
+    {"=[MULTIPLY]=", '*'},
+    {"=[LEFT_PARENTHESIS]=", '('},
+    {"=[RIGHT_PARENTHESIS]=", ')'},
+    {"=[MINUS]=", '-'},
+    {"=[PLUS]=", '+'},
+    {"=[EQUAL]", '='},
+    {"=[LEFT_BRACE]=", '{'},
+    {"=[RIGHT_BRACE]=", '}'},
+    {"=[LEFT_BRACKET]=", '['},
+    {"=[RIGHT_BRACKET]=", ']'},
+    {"=[BACKSLASH]=", '\\'},
+    {"=[COLON]=", ':'},
+    {"=[SEMI]=", ';'},
+    {"=[QUOTE]=", '\"'},
+    {"=[APOSTROPHE]=", '\''},
+    {"=[LESS_THAN]=", '<'},
+    {"=[COMMA]=", ','},
+    {"=[GREATER_THAN]=", '>'},
+    {"=[PERIOD]=", '.'},
+    {"=[QUESTION]=", '?'},
+    {"=[SLASH]=", '/'},
+    {"=[SPACE]=", ' '},
+};
 
 string tokenize(string code) {
 
     string tokened = "";
 
-    bool key_token = 0;
+    bool key = 0;
 
     for (int i = 0; i < code.length(); i++) {
-
-       
+        
         switch (code[i]) {
             case '.':
-                tokened += " [PERIOD] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case ' ':
-                tokened += " [SPACE] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case ';':
-                tokened += " [SEMI] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case ',':
-                tokened += " [COMMA] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case ':':
-                tokened += " [COLLON] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '+':
-                tokened += " [PLUS] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '-':
-                tokened += " [MINUS] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '*':
-                tokened += " [MULTIPLY] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '/':
-                tokened += " [DIVIDE] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '(':
-                tokened += " [LEFT_PARENTHESIS] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case ')':
-                tokened += " [RIGHT_PARENTHESIS] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;                
             case '[':
-                tokened += " [LEFT_BRACKET] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case ']':
-                tokened += " [RIGHT_BRACKET] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '{':
-                tokened += " [LEFT_BRACE] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '}':
-                tokened += " [RIGHT_BRACE] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '&':
-                tokened += " [AMPERSAND] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '^':
-                tokened += " [CARAT] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '%':
-                tokened += " [PERCENT] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '\"':
-                tokened += " [QUOTE] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '\'':
-                tokened += " [APOSTROPHE] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '=':
-                tokened += " [EQUAL] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '$':
-                tokened += " [DOLAR] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '#':
-                tokened += " [HASHTAG] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '@':
-                tokened += " [AT] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '!':
-                tokened += " [EXCLAMATION] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '<':
-                tokened += " [LESS_THAN] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '>':
-                tokened += " [GREATER_THAN] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '?':
-                tokened += " [QUESTION] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
             case '\\':
-                tokened += " [BACKSLASH] ";
-                key_token = 1;
+                tokened += key_tokens[code[i]];
+                key = 1;
                 break;
         }
 
-        if (!key_token) {
+        if (!key) {
             tokened += code[i];
         }
 
-        key_token = 0;
-        
-        
+        key = 0;  
     }
 
     return tokened;
 }
 
+
+
+
 int main(int argc, char *argv[]) {
 
-    cout << tokenize("i'm a skibidi (sigma)") << "\n";
+    cout << tokenize("i'm a. skibidi (sigma)") << "\n";
 
     for (int arg = 0; arg <= argc; arg++) {
 
